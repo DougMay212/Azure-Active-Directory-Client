0db5f2d67ee26b0cf3605d10297b5caf028bb303
May <dmay@lowes.com>
2018-06-06 13:22:40 -0400

    Succeeding in validating Access Tokens passed to the microservice.

--- microservice/src/main/java/com/doug/example/oauthclient/microservice/config/AzureJwtFilter.java
+++ microservice/src/main/java/com/doug/example/oauthclient/microservice/config/AzureJwtFilter.java
@@ -3,41 +3,52 @@ package com.doug.example.oauthclient.microservice.config;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetailsSource;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.GenericFilterBean;
 
-public class AzureJwtFilter extends AbstractAuthenticationProcessingFilter {
+public class AzureJwtFilter extends GenericFilterBean {
 
     private static final String USER_INFO_URL = "https://graph.microsoft.com/v1.0/me/";
     private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
     private static final String TOKEN_PREFIX = "bearer ";
 
-    private UserInfoTokenServices tokenServices;
+    private AzureUserInfoTokenServices tokenServices;
+    private RequestMatcher matcher;
     private AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource =
             new OAuth2AuthenticationDetailsSource();
 
     public AzureJwtFilter(String pattern, String clientId) {
-        super(new AntPathRequestMatcher(pattern));
-        tokenServices = new UserInfoTokenServices(USER_INFO_URL, clientId);
-        this.setAuthenticationDetailsSource(authenticationDetailsSource);
+        super();
+        matcher = new AntPathRequestMatcher(pattern);
+        tokenServices = new AzureUserInfoTokenServices(USER_INFO_URL, clientId);
     }
 
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response)
-            throws AuthenticationException, IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+        if(!matcher.matches(request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         if (request.getHeader(AUTHORIZATION_HEADER_NAME) == null ||
                 !request.getHeader(AUTHORIZATION_HEADER_NAME).toLowerCase().startsWith(TOKEN_PREFIX)) {
             throw new BadCredentialsException("Could not obtain Access Token from Authorization Header");
@@ -45,26 +56,10 @@ public class AzureJwtFilter extends AbstractAuthenticationProcessingFilter {
         String base64AccessToken = request.getHeader(AUTHORIZATION_HEADER_NAME).substring(TOKEN_PREFIX.length());
         OAuth2AccessToken accessToken = new DefaultOAuth2AccessToken(base64AccessToken);
         OAuth2Authentication authentication = this.tokenServices.loadAuthentication(accessToken.getValue());
-        if (this.authenticationDetailsSource != null) {
-            request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_VALUE, accessToken.getValue());
-            request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_TYPE, accessToken.getTokenType());
-            authentication.setDetails(this.authenticationDetailsSource.buildDetails(request));
-        }
-        return authentication;
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult)
-            throws IOException, ServletException {
-        SecurityContextHolder.getContext().setAuthentication(authResult);
-    }
-
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        SecurityContextHolder.clearContext();
-        throw new BadCredentialsException("Token rejected when retrieving user info");
+        request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_VALUE, accessToken.getValue());
+        request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_TYPE, accessToken.getTokenType());
+        authentication.setDetails(this.authenticationDetailsSource.buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
     }
 }
--- microservice/src/main/java/com/doug/example/oauthclient/microservice/config/WebSecurityConfig.java
+++ microservice/src/main/java/com/doug/example/oauthclient/microservice/config/WebSecurityConfig.java
@@ -1,29 +1,41 @@
 package com.doug.example.oauthclient.microservice.config;
 
 import javax.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${azure.client.clientId}")
     private String clientId;
 
+    @Autowired
+    private AzureJwtProvider azureJwtProvider;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.authenticationProvider(azureJwtProvider);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests().anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .addFilterBefore(azureJwtFilter(), BasicAuthenticationFilter.class);
     }
 
     private Filter azureJwtFilter() {
-        //TODO: set rest template for tokenServices?
         return new AzureJwtFilter("/**", clientId);
     }
 }
